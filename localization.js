555f1862464d909eb61f695e84af99330123df5f
Dilfina <68735860+Dilfina@users.noreply.github.com>
2021-08-02 14:23:14 +0600

    Add files via upload

---
+++ localization.js
@@ -0,0 +1,46 @@
+import LocalizedStrings from 'react-native-localization';
+
+export let strings = new LocalizedStrings({
+    kz: {
+        'lang':'Қазақша',
+        'language': 'kz',
+        'main':'Басты',
+        'rsk':'ААК',
+        'news':'Жаңалықтар',
+        'briefing':'Брифинг',
+        'photo':'Сурет',
+        'video':'Видео',
+        'answer':'Сұрақтарға жауап',
+        'contact':'Контактілер'
+
+    
+    },
+    ru: {
+        'lang':'Русский',
+        'language': 'ru',
+        'main':'Главные',
+        'rsk':'РСК',
+        'news':'Новости',
+        'briefing':'Брифинг',
+        'photo':'Фото',
+        'video':'Видео',
+        'answer':'Ответы на вопросы',
+        'contact':'Контакты'
+
+    },
+    en: {
+        'lang':'English',
+        'language': 'en',
+        'main':'Main',
+        'rsk':'RSK',
+        'news':'News',
+        'briefing':'Briefing',
+        'photo':'Photo',
+        'video':'Video',
+        'answer':'Answers for questions',
+        'contact':'Contacts'
+
+
+
+    }
+})
